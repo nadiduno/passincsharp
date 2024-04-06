d7ef4f11720195471a2fc2d6c187de2d9278ca26
nadiduno <nadiduno@gmail.com>
2024-04-06 13:11:39 -0300

    Refactoring exceptions

--- PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -34,17 +34,17 @@ public class RegisterEventUseCase
     {
         if (request.MaximumAttendees <= 0)
         {
-            throw new PassInException("The Maximum attendes is invalid.");
+            throw new ErrorOnValidationException("The Maximum attendes is invalid.");
         }
 
         if (string.IsNullOrWhiteSpace(request.Title))
         {
-            throw new PassInException("The title is invalid.");
+            throw new ErrorOnValidationException("The title is invalid.");
         }
 
         if (string.IsNullOrWhiteSpace(request.Details))
         {
-            throw new PassInException("The title is invalid.");
+            throw new ErrorOnValidationException("The title is invalid.");
         }
     }
 }
